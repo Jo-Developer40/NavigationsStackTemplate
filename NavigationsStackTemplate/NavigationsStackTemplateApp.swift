ce534131471ed841929c09df6b96afee3b77c8eb
Jo-Developer40 <juergen.schulz@me.com>
2025-10-24 11:31:08 +0200

    Initial Commit

---
+++ NavigationsStackTemplate/NavigationsStackTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationsStackTemplateApp.swift
+//  NavigationsStackTemplate
+//
+//  Created by Juergen on 24.10.25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationsStackTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigationsStackTemplateTests/NavigationsStackTemplateTests.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationsStackTemplateTests.swift
+//  NavigationsStackTemplateTests
+//
+//  Created by Juergen on 24.10.25.
+//
+
+import Testing
+
+struct NavigationsStackTemplateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
